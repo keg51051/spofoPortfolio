e88f2620b45523de359701f23ca2b16973546add
Han <keg51051@gmail.com>
2023-09-20 17:51:08 +0900

    :sparkles: [Feat]: StockHave, StockClose Entity, Dto 추가
    클래스 추가

---
+++ src/main/java/spofo/portfolio/domain/stock/dto/StockHaveDto.java
@@ -0,0 +1,24 @@
+package spofo.portfolio.domain.stock.dto;
+
+import java.time.LocalDateTime;
+import lombok.Data;
+import spofo.portfolio.domain.stock.entity.StockHave;
+
+@Data
+public class StockHaveDto {
+
+  private int id;
+  private int portfolioId;
+  private LocalDateTime createdAt;
+  private LocalDateTime modifiedAt;
+  private String stockCode;
+
+  public StockHaveDto(StockHave sh) {
+    id = sh.getId();
+    portfolioId = sh.getPortfolioId();
+    createdAt = sh.getCreatedAt();
+    modifiedAt = sh.getModifiedAt();
+    stockCode = sh.getStockCode();
+  }
+
+}
