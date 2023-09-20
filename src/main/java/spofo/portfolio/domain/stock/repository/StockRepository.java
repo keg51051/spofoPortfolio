f28141225244fc89e0e69319739b635f0717177e
Han <keg51051@gmail.com>
2023-09-20 14:24:18 +0900

    :sparkles: Stock 엔티티, 레포, jpa테스트 추가
    기본적인 stock 베이스 추가

--- src/main/java/spofo/portfolio/domain/stock/repository/StockRepository.java
+++ src/main/java/spofo/portfolio/domain/stock/repository/StockRepository.java
@@ -1,2 +1,11 @@
-package spofo.portfolio.domain.stock.repository;public class StockRepository {
+package spofo.portfolio.domain.stock.repository;
+
+import java.math.BigInteger;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import spofo.portfolio.domain.stock.entity.Stock;
+
+@Repository
+public interface StockRepository extends JpaRepository<Stock, BigInteger> {
+
 }
---
+++ src/test/java/spofo/portfolio/domain/stock/repository/StockRepositoryTest.java
@@ -0,0 +1,19 @@
+package spofo.portfolio.domain.stock.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockRepositoryTest {
+
+  @Autowired
+  StockRepository stockRepository;
+
+  @Test
+  void showAll() {
+    stockRepository.findAll().forEach(System.out::println);
+  }
+}
