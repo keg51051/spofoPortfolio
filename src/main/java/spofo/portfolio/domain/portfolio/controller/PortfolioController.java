d26ac93b2cc5d0271cbb45ee2e9267c9d23194b5
Han <keg51051@gmail.com>
2023-09-22 11:20:46 +0900

    :sparkles: [Feat] : 테스트를 위한 테스트 API

--- src/main/java/spofo/portfolio/domain/portfolio/controller/PortfolioController.java
+++ src/main/java/spofo/portfolio/domain/portfolio/controller/PortfolioController.java
@@ -1,5 +1,17 @@
 package spofo.portfolio.domain.portfolio.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PortfolioController {
+
+    @GetMapping("/test")
+    @ResponseStatus(HttpStatus.OK)
+    public String getStatus() {
+        return "포트폴리오 서버입니다.";
+    }
 
 }
