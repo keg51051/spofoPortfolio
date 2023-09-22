9557b0e7bc04955ece46c38013c61d18fd099565
Han <keg51051@gmail.com>
2023-09-22 17:13:26 +0900

    :wrench: [Config] : Configuration 파일 추가 및 코드 수정

--- src/main/java/spofo/portfolio/domain/stock/controller/StockController.java
+++ src/main/java/spofo/portfolio/domain/stock/controller/StockController.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import spofo.portfolio.domain.stock.dto.request.StockRequest;
@@ -18,10 +20,13 @@ public class StockController {
     @Autowired
     StockService stockService;
 
-    @GetMapping("/")
+    @GetMapping("/portfolios/{portfolioId}/stocks")
     @ResponseStatus(HttpStatus.OK)
-    public List<StockResponse> getStocks() {
-        // TODO : 전체 보유종목 조회
+    public List<StockResponse> getStocks(
+            @RequestParam String keyword,
+            @RequestParam String order,
+            @PathVariable Long portfolioId) {
+        // TODO : 전체 보유 종목 조회
         return stockService.getAllStocks();
     }
 
--- src/main/java/spofo/portfolio/domain/stock/service/StockService.java
+++ src/main/java/spofo/portfolio/domain/stock/service/StockService.java
@@ -2,25 +2,22 @@ package spofo.portfolio.domain.stock.service;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 import spofo.portfolio.domain.stock.dto.response.StockResponse;
-import spofo.portfolio.domain.stock.entity.Stock;
 import spofo.portfolio.domain.stock.repository.StockRepository;
 
 @Service
+@RequiredArgsConstructor
 public class StockService {
 
     private final StockRepository stockRepository;
 
-    public StockService(StockRepository stockRepository) {
-        this.stockRepository = stockRepository;
-    }
-
     public List<StockResponse> getAllStocks() {
         return stockRepository.findAll()
                 .stream()
-                .map(StockResponse::new)
+                .map(StockResponse::from)
                 .collect(Collectors.toList());
     }
 }
