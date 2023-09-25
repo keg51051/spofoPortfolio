49c973dec08bd62ed2fd7ecd8a9637b14814fb97
gayeonP <pagaye@naver.com>
2023-09-25 10:58:28 +0900

    :sparkles: [Feat] : 포트폴리오 자산 조회 개발

--- src/main/java/spofo/global/exception/ErrorCode.java
+++ src/main/java/spofo/global/exception/ErrorCode.java
@@ -7,6 +7,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
+
+    PORTFOLIO_NOT_FOUND(HttpStatus.BAD_REQUEST, "포트폴리오를 찾을 수 없습니다."),
+
     /*
     오류 코드 예시입니다
     DUPLICATED_ID(HttpStatus.BAD_REQUEST, "중복된 회원 아이디 입니다.");
