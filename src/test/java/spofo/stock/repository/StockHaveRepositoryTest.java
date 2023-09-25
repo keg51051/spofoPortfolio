5bca2ce2834af9ebcb25cb2fb32b7b6ce8876c71
h0725j <guswldpqj1@naver.com>
2023-09-25 17:35:24 +0900

    ♻️ [Rename] : 패키지 구조 변경

--- src/test/java/spofo/stock/repository/StockHaveRepositoryTest.java
+++ src/test/java/spofo/stock/repository/StockHaveRepositoryTest.java
@@ -1,9 +1,8 @@
-package spofo.sad.domain.stock.repository;
+package spofo.stock.repository;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import spofo.stock.repository.StockHaveRepository;
 
 @SpringBootTest
 class StockHaveRepositoryTest {
