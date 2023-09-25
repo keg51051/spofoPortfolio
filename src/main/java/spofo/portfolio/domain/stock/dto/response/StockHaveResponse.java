cd38dbc1d5b9a4cfb61b7663083f9b8ff202acd8
Han <keg51051@gmail.com>
2023-09-25 17:43:56 +0900

    :sparkles: [Feat] : api-008의 getQuantity 메소드 구현

--- src/main/java/spofo/portfolio/domain/stock/dto/response/StockHaveResponse.java
+++ src/main/java/spofo/portfolio/domain/stock/dto/response/StockHaveResponse.java
@@ -1,5 +1,6 @@
 package spofo.portfolio.domain.stock.dto.response;
 
+import java.math.BigDecimal;
 import lombok.Builder;
 import org.jetbrains.annotations.NotNull;
 import spofo.portfolio.domain.stock.entity.StockHave;
@@ -11,11 +12,40 @@ public class StockHaveResponse {
     private String stockCode;
     private Long portfolioId;
 
-    public static StockHaveResponse from(@NotNull StockHave sh) {
+    private String stockName;
+    private String sector;
+    private BigDecimal totalAsset;
+    private BigDecimal gain;
+    private BigDecimal gainRate;
+    private BigDecimal avgPrice;
+    private BigDecimal currentPrice;
+    private int quantity;
+    private String imageUrl;
+
+    public static StockHaveResponse from(
+            @NotNull StockHave sh,
+            String stockName,
+            String sector,
+            BigDecimal totalAsset,
+            BigDecimal gain,
+            BigDecimal gainRate,
+            BigDecimal avgPrice,
+            BigDecimal currentPrice,
+            BigDecimal quantity,
+            String imageUrl) {
         return StockHaveResponse.builder()
                 .id(sh.getId())
                 .stockCode(sh.getStockCode())
                 .portfolioId(sh.getPortfolioId())
+                .stockName(stockName)
+                .sector(sector)
+                .totalAsset(totalAsset)
+                .gain(gain)
+                .gainRate(gainRate)
+                .avgPrice(avgPrice)
+                .currentPrice(currentPrice)
+                .quantity(quantity)
+                .imageUrl(imageUrl)
                 .build();
     }
 }
