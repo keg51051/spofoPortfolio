4310db04025fc4646688b7fa70d775894aa6b44b
h0725j <guswldpqj1@naver.com>
2023-09-26 15:35:49 +0900

    🎨 [Style] : PR 리뷰를 바탕으로 코드 수정

--- src/main/java/spofo/portfolio/controller/PortfolioController.java
+++ src/main/java/spofo/portfolio/controller/PortfolioController.java
@@ -4,13 +4,12 @@ import static org.springframework.http.ResponseEntity.ok;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import spofo.portfolio.dto.request.CreatePortfolioRequest;
 import spofo.portfolio.dto.response.CreatePortfolioResponse;
@@ -41,30 +40,11 @@ public class PortfolioController {
 
     @PostMapping("/portfolios")
     public ResponseEntity<CreatePortfolioResponse> createPortfolio(
-            @RequestBody CreatePortfolioRequest createPortfolioRequest) {
+            @RequestBody @Validated CreatePortfolioRequest createPortfolioRequest) {
         CreatePortfolioResponse createPortfolioResponse = portfolioService.createPortfolio(
                 createPortfolioRequest);
         return ok(createPortfolioResponse);
     }
-
-    @GetMapping("/test")
-    @ResponseStatus(HttpStatus.OK)
-    String getStatus() {
-        return "포트폴리오 서버입니다.";
-    }
-
-    @GetMapping("/test/callStock")
-    @ResponseStatus(HttpStatus.OK)
-    String getStock() {
-        return portfolioService.getStock();
-    }
-
-    @GetMapping("/test/callAuth")
-    @ResponseStatus(HttpStatus.OK)
-    String getAuth() {
-        return portfolioService.getAuth();
-    }
-
 
     @GetMapping("/portfolios/{portfolioId}/total")
     public PortfolioResponse getPortfolio(@PathVariable String portfolioId) {
