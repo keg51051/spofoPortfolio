efe83a2decf15e7dcf50cd16ddd4e87bccfd64d4
Han <keg51051@gmail.com>
2023-10-05 17:15:25 +0900

    :sparkles: [Feat] : Stock RestClient & 서비스 로직 구현
    리팩토링 할 예정이므로 로직과 코드 구조는 바뀔 예정

--- src/main/java/spofo/stock/controller/StockController.java
+++ src/main/java/spofo/stock/controller/StockController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,9 +42,28 @@ public class StockController {
     public ResponseEntity<AddStockResponse> addStock(
             @RequestBody @Validated AddStockRequest addStockRequest,
             @PathVariable("portfolioId") Long portfolioId) {
+        // TODO : 종목 매수
         AddStockResponse addStockResponse = stockHaveService.addStock(
                 addStockRequest, portfolioId);
         return ok(addStockResponse);
+    }
+
+//    @PostMapping("/portfolios/{portfolioId}/stocks/{stockId}")
+//    public ResponseEntity<AddStockResponse> addMoreStock(
+//            @RequestBody @Validated AddStockRequest addStockRequest,
+//            @PathVariable("portfolioId") Long portfolioId,
+//            @PathVariable("stockId") Long stockId) {
+//        // TODO : 종목 추가 매수
+//        AddStockResponse addStockResponse = stockHaveService.addMoreStock(addStockRequest);
+//        return ok(addStockResponse);
+//    }
+
+    @DeleteMapping("/portfolios/{portfolioId}/stocks/{stockId}")
+    public ResponseEntity<StockHaveResponse> deleteStock(
+            @PathVariable("portfolioId") Long portfolioId,
+            @PathVariable("stockId") Long stockId) {
+        // TODO : 종목 삭제
+        return null;
     }
 
 }
