697f6538e92179a0e6da6d8855c1eb939e2d4db9
gayeonP <pagaye@naver.com>
2023-10-06 12:07:05 +0900

    :sparkles: [Feat] : CORS 해결을 위한 localhost 접속 허용

--- src/main/java/spofo/global/config/WebConfig.java
+++ src/main/java/spofo/global/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**") // 프로그램에서 제공하는 URL
-                .allowedOrigins("http://spofo.net") // 청을 허용할 출처를 명시, 전체 허용 (가능하다면 목록을 작성한다.
+                .allowedOrigins("http://spofo.net", "http://localhost:5173/") // 청을 허용할 출처를 명시, 전체 허용 (가능하다면 목록을 작성한다.
 //                .allowedHeaders("*") // 어떤 헤더들을 허용할 것인지
                 .allowedMethods("*") // 어떤 메서드를 허용할 것인지 (GET, POST...)
                 .allowCredentials(
